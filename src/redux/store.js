f8c4dc0f6c7940e417d5016d8c68d5bcf26f27dd
Nathiya-R <146801806+Nathiya-R@users.noreply.github.com>
2024-04-05 14:08:56 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import{
+  persistStore,
+  PersistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+}from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import farmerReducer from '../redux/farmerSlice'
+const persistConfig={
+  key:"root",
+  version:1,
+  storage,
+};
+const PersistReducer=PersistReducer(persistConfig,rootReducer);
 export const store = configureStore({
-  reducer: {farmerReducer},
-})
+  reducer: PersistReducer,
+  middleware:(getDefaultMiddleware)=>
+    getDefaultMiddleware({
+      serializableCheck:{
+        ignoredActions:[FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER],
+      },
+    }),
+});
+
+export let persistor=persistStore(store);
