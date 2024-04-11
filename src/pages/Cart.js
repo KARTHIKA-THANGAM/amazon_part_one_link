62076e1be32a2947d354fdd066b721736319eb1f
Nathiya-R <146801806+Nathiya-R@users.noreply.github.com>
2024-04-11 11:51:57 -0600

    Update Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,6 @@
 import React ,{useEffect,useState }from 'react';
 import {useDispatch,useSelector} from 'react-redux';
+import {Link} from 'react-router-dom';
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { decrementQuantity, deleteItem ,incrementQuantity,resetCart} from '../redux/farmerSlice';
 import { emptyCart } from '../assets/index';
